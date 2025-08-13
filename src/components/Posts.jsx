7485f3953fde6372f69ca879ef64d15477182a9e
Ahmed Salah <ahmedsalah5446@gmail.com>
2025-08-13 19:19:28 +0300

    components

---
+++ src/components/Posts.jsx
@@ -0,0 +1,45 @@
+import NewPost from "./NewPost";
+import classes from './Posts.module.css';
+import Modal from "./Modal";
+import { useState } from "react";
+import Post from "./Post";
+
+function Posts(props) {
+    const [posts, setPosts] = useState([]);
+
+    function addPostHandler(postData) {
+        fetch('http://localhost:8080/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(postData)
+        })
+        // arrow function cuz the current state depends on the previous state 
+        setPosts((prevPosts) => [postData, ...prevPosts]);
+    }
+
+    
+
+    return (
+        <>
+            {props.isPosting && <Modal onClose={props.onStopPost}>
+                <NewPost 
+                    onCancel={props.onStopPost}
+                    onAddPost={addPostHandler}
+                />
+            </Modal>}
+            {/* hide when there is no posts */}
+            {posts.length > 0 && <ul className={classes.posts}>
+                {posts.map((post, index) => (
+                    <Post key={index} author={post.author} body={post.body} />
+                ))}
+            </ul>}
+            
+            {posts.length == 0 && <div style={{textAlign: 'center'}}>
+                <h2>There are no posts yet. <br></br> Start adding some</h2></div>}
+        </>
+    )
+}
+
+export default Posts;
